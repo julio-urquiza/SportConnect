843dbd34c5aafe214fe91573da1eab8c753789be
julio-urquiza <julio.cesar.uquiza4@gmail.com>
2026-05-12 01:06:32 -0300

    feat: update server.js to use await for database connection and add .gitkeep files for directory structure

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app.js'
 import dotenv from 'dotenv'
 import connectDatabase from './config/dataBase.js'
 
-connectDatabase()
+await connectDatabase()
 app.listen(process.env.PORT,()=> console.log(`Server escuchando: ${process.env.PORT}`))
