cc4efd9bbe5b3430b30f3ec358e7512d9c3f1d02
TomasPugente <pugentetomas@gmail.com>
2026-05-21 14:30:32 -0300

    feat: remove .env.Example from repository and update .gitignore; add user-service.js file

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -2,7 +2,7 @@ import UsuarioDao from "../daos/mongoDB/usuario-dao"
 
  registrarUsuario = async(req,res)=>{
     try {
-        
+        const response= this.ser
     } catch (error) {
         throw new error(error);
     }
